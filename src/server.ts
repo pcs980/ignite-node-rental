58aad31bc0c100bc98abeebd2be9b2ae138eb43a
Paulo César da Silva <paulocsmg@gmail.com>
2021-05-18 17:44:48 -0300

    Debugger

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,11 @@
 import express from 'express';
 
 const app = express();
+app.use(express.json());
+
+app.post('/courses', (request, response) => {
+  const { name } = request.body;
+  return response.json({ name });
+});
 
 app.listen(3333, () => console.log('Server is running'));
