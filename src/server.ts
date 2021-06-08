5a37fc879ba052b23c69f40790b3e322efbaf64b
Paulo César da Silva <paulocsmg@gmail.com>
2021-06-08 14:43:07 -0300

    Dockerized

--- src/server.ts
+++ src/server.ts
@@ -8,4 +8,4 @@ app.use(express.json());
 app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerFile));
 app.use(router);
 
-app.listen(3333, () => console.log('Server is running'));
+app.listen(3333, () => console.log('Server is running on port', 3333));
